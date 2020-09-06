6adc0aab898fe88994ee6e722f4dfee8e4465bfb
Butch Landingin <butchland@gmail.com>
2020-09-06 19:02:58 +0800

    Update link to course19.fast.ai where course-v3 has moved

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ enable_index_page = True
 api_response_key_name = 'result'
 
 # configuration for image learner pkl file stored in cloud
-# see https://course.fast.ai/deployment_render.html for instructions to generate download urls
+# see https://course19.fast.ai/deployment_render.html for instructions to generate download urls
 # for model files (.pkl) shared on google drive and dropbox
 model_file_download_url = 'https://drive.google.com/uc?export=download&id=1-OXLsznNXsAvexiLHaBMoEQ4-N6N-t_4'
 model_file_name = 'export.pkl'
