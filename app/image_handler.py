4169339211b4e33952200aa1496b1f9c63cb5f7d
Butch Landingin <butchland@gmail.com>
2020-09-07 17:47:19 +0800

    Fix code to match updated method arguments

--- app/image_handler.py
+++ app/image_handler.py
@@ -1,6 +1,10 @@
 
 from io import BytesIO
-from fastai.vision import open_image
+import warnings
+from fastai.vision.all import PILImage
+def open_image(fn:str)->PILImage:
+    "Return `Image` object created from image in file `fn`."
+    return PILImage.create(fn)
 
 async def get_uploaded_image(request):
     data = await request.form()
